d633707e7c7b7e86f520f8dc7ed5ede756dbc7a0
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-05-11 21:09:47 +0530

    Create Min_and_Max.java

---
+++ Array/Min_and_Max.java
@@ -0,0 +1,35 @@
+/* Problem Statement:
+Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+Example 1:
+
+Input:
+N = 6
+A[] = {3, 2, 1, 56, 10000, 167}
+Output:
+min = 1, max =  10000
+*/
+
+//SOLUTION:
+
+/*
+ class pair  
+{  
+    long first, second;  
+    public pair(long first, long second)  
+    {  
+        this.first = first;  
+        this.second = second;  
+    }  
+} */
+
+class Compute 
+{
+    static pair getMinMax(long a[], long n)  
+    {
+       Arrays.sort(a);
+       long min=a[0];
+       long max=a[(int)n-1];
+       return new pair(min,max);
+    }
+}
