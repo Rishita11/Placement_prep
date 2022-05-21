26e9775fc7f2575efff0da91bd789a2a60c4fab9
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-05-21 13:14:51 +0530

    Create Union_of_two_array.java

---
+++ Array/Union_of_two_array.java
@@ -0,0 +1,32 @@
+/*
+Given two arrays a[] and b[] of size n and m respectively. The task is to find union between these two arrays.
+
+Union of the two arrays can be defined as the set containing distinct elements from both the arrays. If there are repetitions, then only one occurrence of element should be printed in the union.
+
+Example 1:
+
+Input:
+5 3
+1 2 3 4 5
+1 2 3
+Output: 
+5
+Explanation: 
+1, 2, 3, 4 and 5 are the
+elements which comes in the union set
+of both arrays. So count is 5.
+*/
+
+class Solution{
+    public static int doUnion(int a[], int n, int b[], int m) 
+    {
+        Set<Integer> l=new HashSet<>();
+        for(int x:a){
+           l.add(x);
+        }
+        for(int x:b){
+           l.add(x);
+        }
+        return l.size();
+    }
+}
