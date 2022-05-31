38570266b85b9be916c66190a65ca8cd6b1fd994
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-05-31 23:31:41 +0530

    Create Sort_colours.java

---
+++ Striver_SDE_Sheet/Sort_colours.java
@@ -0,0 +1,57 @@
+/*
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+*/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int temp;
+        int l=0;
+        int m=0;
+        int h=n-1;
+       while(m<=h){
+           switch(nums[m]){
+               case 0:
+                   {
+                      temp=nums[l];
+                       nums[l]=nums[m];
+                       nums[m]=temp;
+                       m++;
+                       l++;
+                       break;
+                   }
+               case 1:
+                   {
+                       m++;
+                       break;
+                   }
+               case 2:
+                   {
+                       temp=nums[m];
+                       nums[m]=nums[h];
+                       nums[h]=temp;
+                       h--;
+                     
+                       break;
+                   }
+           }
+         }
+  }
+        
+}
