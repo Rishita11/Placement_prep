180dae09524e4842655bab28a2265e0184b8c94d
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-06-02 00:28:36 +0530

    Update Roman_to_Integer.java

--- String/Roman_to_Integer.java
+++ String/Roman_to_Integer.java
@@ -21,3 +21,26 @@ s = III
 Output: 3
 
 */
+class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+       int res=0;
+       HashMap <Character,Integer> num=new HashMap<>();
+       num.put('I',1);
+       num.put('V',5);
+       num.put('X',10);
+       num.put('L',50);
+       num.put('C',100);
+       num.put('D',500);
+       num.put('M',1000);
+       
+       for(int i=0;i<str.length();i++){
+           if(i>0 && num.get(str.charAt(i))>num.get(str.charAt(i-1)))
+            res+=num.get(str.charAt(i))-2*num.get(str.charAt(i-1));
+           else
+           res+=num.get(str.charAt(i));
+          
+       }
+       return res;
+       }
+}
