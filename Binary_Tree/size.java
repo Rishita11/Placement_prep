7cb49dfb4e79ca9e9016f81d266ae073044714e0
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-06-29 00:45:06 +0530

    Create size.java

---
+++ Binary_Tree/size.java
@@ -0,0 +1,32 @@
+/*
+Given a binary tree of size N, you have to count number of nodes in it. For example, count of nodes in below tree is 4.
+
+        1
+     /      \
+   10      39
+  /
+5
+
+Input:
+First line of input contains the number of test cases T. For each test case, there will be only a single line of input which is a string representing the tree as described below: 
+
+The values in the string are in the order of level order traversal of the tree where, numbers denote node values, and a character “N” denotes NULL child.
+
+For example:
+
+For the above tree, the string will be: 1 2 3 N N 4 6 N 5 N N 7 N
+
+Output:
+For each testcase in new line, print the number of nodes.
+
+*/
+
+class Tree
+{
+	public static int getSize(Node root)
+	{
+        if(root==null)
+        return 0;
+        return (getSize(root.left)+getSize(root.right)+1);
+    }
+}
