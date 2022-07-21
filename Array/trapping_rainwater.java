26ca22b5437284cc916f54c19c613e98472411ca
Rishita Srivastava <66817759+Rishita11@users.noreply.github.com>
2022-07-22 01:22:15 +0530

    Create trapping_rainwater.java

---
+++ Array/trapping_rainwater.java
@@ -0,0 +1,35 @@
+/*
+Given an array arr[] of N non-negative integers representing the height of blocks. If width of each block is 1, compute how much water can be trapped between the blocks during the rainy season. 
+ 
+
+Example 1:
+
+Input:
+N = 6
+arr[] = {3,0,0,2,0,4}
+Output:
+10*/
+
+class Solution{
+    
+    // arr: input array
+    // n: size of array
+    // Function to find the trapped water between the blocks.
+    static long trappingWater(int arr[], int n) { 
+        long res=0;
+        int left[]=new int[n];
+        int right[]=new int[n];
+        left[0]=arr[0];
+        for(int i=1;i<n;i++){
+            left[i]=Math.max(left[i-1],arr[i]);
+        }
+       right[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=Math.max(right[i+1],arr[i]);
+        }
+        for(int i=0;i<n;i++){
+            res+=(Math.min(left[i],right[i])-arr[i]);
+        }
+        return res;
+    } 
+}
